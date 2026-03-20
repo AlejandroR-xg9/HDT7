0c5d2421f92202a39682f5d91675afb32b73acfa
AlejandroR <rus25512@uvg.edu.gt>
2026-03-20 16:29:26 -0600

    Add files via upload
    Association class

---
+++ Association.java
@@ -0,0 +1,28 @@
+public class Association<k extends Comparable<k>, v> implements Comparable<Association<k, v>> {
+    
+private k key;
+private v value;
+
+public Association(k key, v value) {
+    this.key = key;
+    this.value = value;
+}
+public k getKey() {
+    return key;
+    }
+public v getValue() {
+    return value;
+    }
+public void setKey(k key) {
+    this.key = key;
+    }
+public void setValue(v value) {
+    this.value = value;
+
+    }
+
+@Override
+    public int compareTo(Association<k, v> other) {
+        return this.key.compareTo(other.key);
+    }
+}
